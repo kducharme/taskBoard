bd7d0f9ac4fdbd0dd4569bae7b185058f3124d84
Kyle Ducharme <ducharme.kyle@gmail.com>
2018-04-21 16:04:03 -0500

    hooking up firebase

--- styling/tasksDB.js
+++ styling/tasksDB.js
@@ -1,1 +1,16 @@
-const 
+const tasksDB = () => {
+    $.ajax({
+        url: 'https://kyle-personal-blog.firebaseio.com/posts.json',
+        type: "POST",
+        data: JSON.stringify(newPost),
+        success: function () {
+            console.log("success");
+        },
+        error: function (error) {
+            console.log("error: " + error)
+        }
+    });
+}
+
+
+https://task-list-cf398.firebaseio.com/
